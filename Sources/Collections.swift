277418e0a863af44bd2c8e2f9cd87f9815bba77f
Adam Nemecek <adamnemecek@gmail.com>
2016-12-03 07:29:42 -0800

    v 0.01

---
+++ Sources/Collections.swift
@@ -0,0 +1,60 @@
+//
+//  Collections.swift
+//  WarpKit
+//
+//  Created by Adam Nemecek on 10/1/16.
+//  Copyright © 2016 Adam Nemecek. All rights reserved.
+//
+
+import Foundation
+
+extension Sequence {
+    public func all(predicate: (Iterator.Element) -> Bool) -> Bool {
+        for e in self where !predicate(e) {
+            return false
+        }
+        return true
+    }
+
+    public func any(predicate: (Iterator.Element) -> Bool) -> Bool {
+        for e in self where predicate(e) {
+            return true
+        }
+        return false
+    }
+
+    public func none(predicate: (Iterator.Element) -> Bool) -> Bool {
+        for e in self where predicate(e) {
+            return false
+        }
+        return true
+    }
+}
+
+extension RangeReplaceableCollection where Iterator.Element: ExpressibleByIntegerLiteral {
+	/// Initialize array with zeroes, ~10x faster than append for array of size 4096
+	///
+	/// - parameter count: Number of elements in the array
+	///
+
+    public init(zeros count: Int) {
+        self.init(repeating: 0, count: count)
+    }
+}
+
+extension Sequence where Iterator.Element: Hashable {
+    public var unique: [Iterator.Element] {
+        var s: Set<Iterator.Element> = []
+        return filter { s.insert($0).inserted }
+    }
+}
+
+extension Collection where IndexDistance == Int {
+    /// Return a random element from the collection
+	public func random() -> Iterator.Element {
+		let offset = Int(arc4random_uniform(UInt32(count.toIntMax())))
+		return self[index(startIndex, offsetBy: offset)]
+	}
+}
+
+
---
+++ Sources/RangeType.swift
@@ -0,0 +1,26 @@
+//
+//  RangeType.swift
+//  WarpKit
+//
+//  Created by Adam Nemecek on 12/3/16.
+//
+//
+
+import Foundation
+
+public protocol RangeType {
+    associatedtype Bound: Comparable
+    var lowerBound: Bound { get }
+    var upperBound: Bound { get }
+}
+
+extension RangeType {
+    public func clamp(_ value: Bound) -> Bound {
+        return min(max(value, lowerBound), upperBound)
+    }
+}
+
+extension Range: RangeType { }
+extension CountableRange: RangeType { }
+extension ClosedRange: RangeType { }
+extension CountableClosedRange: RangeType { }
